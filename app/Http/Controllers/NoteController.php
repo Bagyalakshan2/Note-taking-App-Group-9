95382b8e849abdfbf59ba71a543003e7d2070074
Bhagya Lakshan <168979460+Bagyalakshan2@users.noreply.github.com>
2024-10-23 19:53:05 +0530

    Update NoteController.php

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -11,11 +11,7 @@ use Illuminate\Http\Request;
 
 class NoteController extends Controller
 {
-    public function index()
-    {
-        $notes = Note::all(); // Retrieve all notes from the database
-        return view('notes.index', compact('notes')); // Pass notes to the view
-    }
+   
 
     public function create()
     {
